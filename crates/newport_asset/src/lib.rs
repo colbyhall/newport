0be3349426660937123f75c2074c27728224c0ab
Colby <me@cobeh.com>
2021-05-26 20:06:43 -0700

    uuid

--- crates/newport_asset/src/lib.rs
+++ crates/newport_asset/src/lib.rs
@@ -1,13 +1,14 @@
 #![feature(box_syntax)]
 #![feature(trait_alias)]
+#![feature(string_remove_matches)]
 //! This crate provides a completely thread safe asset manager which 
 //! handles defining assets, loading assets, ref counting assets, and 
 //! serialization.
 
-
 pub(crate) use newport_engine as engine;
-pub(crate) use newport_log as log;
-pub(crate) use newport_cache as cache;
+pub(crate) use newport_log    as log;
+pub(crate) use newport_cache  as cache;
+pub(crate) use newport_serde  as serde;
 
 pub use std::path::{ Path, PathBuf };
 
@@ -18,8 +19,14 @@ mod asset;
 mod asset_manager;
 mod asset_ref;
 mod registers;
+mod asset_cache;
+mod uuid;
 
-pub use asset::*;
-pub use asset_manager::*;
-pub use asset_ref::*;
-pub use registers::*;
+pub use {
+    asset::*,
+    asset_manager::*,
+    asset_ref::*,
+    registers::*,
+    asset_cache::*,
+    uuid::*,
+};
---
+++ crates/newport_asset/src/uuid.rs
@@ -0,0 +1,102 @@
+use crate::{
+    serde,
+};
+
+use serde::{
+    Serialize,
+    Serializer,
+
+    Deserialize,
+    Deserializer,
+    de::{ self, Visitor },
+};
+
+use std::fmt;
+
+#[derive(Copy, Clone, PartialEq, PartialOrd, Hash, Debug)]
+pub struct UUID {
+    a: u32,
+    b: u16,
+    c: u16,
+    d: u64,
+}
+
+impl Serialize for UUID {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let d = self.d >> 48;
+        let e = self.d & 0x0000FFFFFFFFFFFF;
+        let mut out = format!("{{{:#8x}-{:#4x}-{:#4x}-{:#4x}-{:#12x}}}", self.a, self.b, self.c, d, e);
+        out.remove_matches("0x");
+        serializer.serialize_str(&out)
+    }
+}
+
+struct UUIDString;
+
+impl<'de> Visitor<'de> for UUIDString {
+    type Value = UUID;
+
+    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        formatter.write_str("an string assembled as such {00000000-0000-0000-0000-000000000000}")
+    }
+
+    fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
+    where
+        E: de::Error,
+    {
+        if !v.starts_with('{') || !v.ends_with('}') {
+            return Err(de::Error::invalid_type(de::Unexpected::Str(v), &self));
+        }
+
+        let v = v.strip_prefix('{').unwrap();
+        let v = v.strip_suffix('}').unwrap();
+        
+        let values: Vec<&str> = v.split('-').collect();
+        if values.len() != 5 {
+            return Err(de::Error::invalid_type(de::Unexpected::Str(v), &self));
+        }
+
+        let a: u32 = u32::from_str_radix(values[0], 16).map_err(|_| de::Error::invalid_type(de::Unexpected::Str(values[0]), &self))?;
+        let b: u16 = u16::from_str_radix(values[1], 16).map_err(|_| de::Error::invalid_type(de::Unexpected::Str(values[1]), &self))?;
+        let c: u16 = u16::from_str_radix(values[2], 16).map_err(|_| de::Error::invalid_type(de::Unexpected::Str(values[2]), &self))?;
+
+        let d0: u16 = u16::from_str_radix(values[3], 16).map_err(|_| de::Error::invalid_type(de::Unexpected::Str(values[3]), &self))?;
+        let d1: u64 = u64::from_str_radix(values[4], 16).map_err(|_| de::Error::invalid_type(de::Unexpected::Str(values[4]), &self))?;
+
+        let d = ((d0 as u64) << 48) | d1;
+
+        Ok(Self::Value{ a, b, c, d })
+    }
+}
+
+impl<'de> Deserialize<'de> for UUID {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+            D: Deserializer<'de> {
+        deserializer.deserialize_str(UUIDString)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn serialize() {
+        let uuid = UUID{ a: 0x123456, b: 0x789A, c: 0xBCDE, d: 0xF0123456789ABCDE };
+
+        let serialized = serde::ron::to_string(&uuid).unwrap();
+        assert_eq!(serialized, "\"{123456-789a-bcde-f012-3456789abcde}\"");
+    }
+
+    #[test]
+    fn deserialize() {
+        let uuid_string = "\"{123456-789a-bcde-f012-3456789abcde}\"";
+
+        let deserialized: UUID = serde::ron::from_str(uuid_string).unwrap();
+        assert_eq!(deserialized, UUID{ a: 0x123456, b: 0x789A, c: 0xBCDE, d: 0xF0123456789ABCDE });
+    }
+}
