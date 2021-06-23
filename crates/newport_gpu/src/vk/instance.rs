cbbf47c7775e2394ff3239058cedbb8906a49f8c
Colby <me@cobeh.com>
2021-06-22 19:40:31 -0700

    proper vulkan version

--- crates/newport_gpu/src/vk/instance.rs
+++ crates/newport_gpu/src/vk/instance.rs
@@ -22,7 +22,7 @@ impl Instance {
         };
 
         let app_info = vk::ApplicationInfo{
-            api_version: vk::make_version(1, 0, 0),
+            api_version: vk::make_version(1, 2, 0),
             ..Default::default()
         };
 
