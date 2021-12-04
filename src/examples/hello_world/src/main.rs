f50e0f87cd56b44c5ea54dae5084aa23b746144b
Colby <me@cobeh.com>
2021-12-03 16:34:25 -0800

    removed reference to gui lib

--- src/examples/hello_world/src/main.rs
+++ src/examples/hello_world/src/main.rs
@@ -1,30 +1,13 @@
-use {
-	engine::{
-		define_run_module,
-		Builder,
-		Module,
-	},
-	gui::*,
+use engine::{
+	define_run_module,
+	Module,
 };
 
 struct HelloWorld;
 
 impl Module for HelloWorld {
 	fn new() -> Self {
-		let button = Border::new(|slot| {
-			slot.child(Button::new(|slot| {
-				slot.child(TextBlock::new("Hello World"))
-			}))
-		});
-		let widgets = Gui::widgets_mut();
-		let button = widgets.create(button);
-		Gui::widgets_mut().set_base(Some(button));
-
 		Self
-	}
-
-	fn depends_on(builder: Builder) -> Builder {
-		builder.module::<Gui>()
 	}
 }
 
